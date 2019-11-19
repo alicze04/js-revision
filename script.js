47945bd872b7757b79d8716aabcd37a63f22dc9b
Alicja <43991946+alicze04@users.noreply.github.com>
2019-11-19 17:56:09 +0100

    Add files via upload
    Zad2 files

---
+++ script.js
@@ -0,0 +1,5 @@
+ //2. Write a JavaScript program to print the contents of the current window.
+ //Tutaj musialam wrzucic event 'onclick' bezposrednio do pliku HTML, bo w samym pliku JS nie dzialal, nie drukowal strony po kliknieciu na button, tylko przy odswiezaniu strony..w HTML wszystko dzialalo jak nalezy, na przycisk...Ciekawe dlaczego ? ...
+
+// document.getElementById("printButton").onclick = window.print();
+
